23f2ec1185e823afe2bbd4365811c1e57a2c06f3
yjhwo <yjhwo@naver.com>
2016-04-12 17:57:28 +0900

    0412

---
+++ WebContent/hello.js
@@ -0,0 +1,8 @@
+document.write("<strong>Hello World</strong>");
+//alert("Hello World");
+
+console.log("Hello World");
+console.error("error!!!");
+console.warn("warning!!!");
+console.info("info...");
+/* alert보다 디버깅 용으로 더 좋음 */
